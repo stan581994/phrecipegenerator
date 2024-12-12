044db04e8b6388c3d1bdf383392f77321772ba79
steven-tan-telus <steven.tan@telus.com>
2024-12-12 15:56:35 -0500

    host set to true to allow server to be accessible externally

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+    host: true, // This will allow the server to be accessible externally
   },
 })
